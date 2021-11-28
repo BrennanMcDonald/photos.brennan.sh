4788086cee04038e4a56473ee0957f3d0abdaa83
BrennanMcDonald <Brennan.mcdonald.mail@gmail.com>
2021-11-28 22:52:23 +0000

    Deploying to gh-pages from  @ 638c6ac21d5662d57a9bfd1a37f58bcbcd4cc1eb 🚀

---
+++ js/heatmap-data.js
@@ -0,0 +1,23 @@
+(() => {
+  // ns-params:@params
+  var data = {};
+
+  // <stdin>
+  if (data) {
+    const calData = Object.keys(data).map((date) => ({
+      date,
+      total: data[date],
+      details: [],
+      summary: []
+    }));
+    const div_id = "calendar";
+    const label = "Photos";
+    const color = "#cd2327";
+    const overview = "global";
+    const handler = function(val) {
+      console.log(val);
+      window.location = "/about";
+    };
+    calendarHeatmap.init(calData, div_id, color, overview, handler, label);
+  }
+})();
---
+++ js/random.min.ac6574daa16ca332d92237acd62df9c4b831d6debc06f65784b5154e7b80b136.js
@@ -0,0 +1,1 @@
+(()=>{var a=[];window.location=a[Math.floor(Math.random()*a.length)]})()
